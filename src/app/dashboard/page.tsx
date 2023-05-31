6c56fe4405adee64566cfbd913a0a6f9e1ff0aed
RJ Villanueva <rj.villanueva89@gmail.com>
2023-05-31 11:11:50 +0800

    experimented on pages and layouts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World!
+    </div>
+  );
+};
+
+export default Dashboard;
